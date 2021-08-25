923214dd37f916990d6d214ea5fa36a1278074c6
geraldoahnert <geraldoahnert@gmail.com>
2021-08-25 18:30:01 -0300

    feat: initial server setup express

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(5000, () => {
+  console.log('+ Server started at port: 5000');
+});
