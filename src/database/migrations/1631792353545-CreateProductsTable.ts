e0e48ff398ad1ed64f35fa3db99333da020c991f
Geraldo Ahnert <geraldoahnert@gmail.com>
2021-09-16 10:28:22 -0300

    feat: create products migration

---
+++ src/database/migrations/1631792353545-CreateProductsTable.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProductsTable1631792353545 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'product',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'price',
+            type: 'varchar',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
