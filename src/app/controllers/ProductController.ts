5f622953de4539cc939e8d8222928f3c7c502c43
Geraldo Ahnert <geraldoahnert@gmail.com>
2021-09-23 15:47:34 -0300

    feat: product delete controller

--- src/app/controllers/ProductController.ts
+++ src/app/controllers/ProductController.ts
@@ -2,6 +2,21 @@ import { Request, Response } from 'express';
 import { ProductService } from '../services/ProductService';
 
 class ProductController {
+  async remove(req: Request, res: Response) {
+    const id = req.params.id;
+
+    const productService = new ProductService();
+
+    const deleteProduct = await productService.remove({
+      id,
+    });
+
+    return res.json({
+      Status: 'Product has been deleted.',
+      Product: deleteProduct,
+    });
+  }
+
   async index(req: Request, res: Response) {
     const id = req.params.id;
 
