e0ee38010a39c7ab4c95646ce0938011dc1a0ebc
Geraldo Ahnert <geraldoahnert@gmail.com>
2021-10-18 18:39:18 -0300

    fix: new product routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,32 +1,22 @@
 import { Router } from 'express';
-
 import { UserController } from './app/controllers/UserController';
-
 import { AuthController } from './app/controllers/AuthController';
-
-import { ProductRegisterController } from './app/controllers/ProductRegisterController';
-import { ProductShowController } from './app/controllers/ProductShowController';
-import { ProductDeleteController } from './app/controllers/ProductDeleteController';
-
+import { ProductController } from './app/controllers/ProductController';
 import authMiddleware from './app/middlewares/authMiddleware';
 
 const router = Router();
 
 const userController = new UserController();
-
 const authController = new AuthController();
-
-const productRegisterController = new ProductRegisterController();
-const productShowController = new ProductShowController();
-const productDeleteController = new ProductDeleteController();
+const productController = new ProductController();
 
 router.post('/users', userController.handle);
 router.get('/users', authMiddleware, userController.handle);
 
 router.post('/login', authController.handle);
 
-router.post('/product', productRegisterController.handle);
-router.get('/product/:id', productShowController.handle);
-router.delete('/product/:id', productDeleteController.handle);
+router.post('/product', productController.create);
+router.get('/product/:id', productController.show);
+router.delete('/product/:id', productController.delete);
 
 export default router;
